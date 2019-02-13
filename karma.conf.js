54e6c344ad4c8edc201c7191abbd899a37cec40e
eecheverry <eecheverry@nubark.com>
2019-02-13 15:19:13 -0500

    stage karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,36 @@
-const path = require('path')
-
 module.exports = function (config) {
   config.set({
     basePath: '.',
     frameworks: ['jasmine'],
     files: [
-      {pattern: 'lib/**/*.js', type: 'module', watched: true, included: true, served: true},
-      {pattern: 'tests/**/*.js', type: 'module', watched: true, included: true, served: true}
+      { pattern: 'lib/**/*.js', type: 'module', watched: true, included: true, served: true },
+      { pattern: 'tests/**/*.js', type: 'module', watched: true, included: true, served: true }
     ],
+    // files: [
+    //   'lib/**/*.js',
+    //   'tests/**/*.js'
+    // ],
     plugins: [
       'karma-jasmine',
+      'karma-coverage',
       'karma-chrome-launcher',
       'karma-babel-preprocessor'
     ],
     reporters: ['progress', 'coverage'],
     preprocessors: {
-      'lib/**/*.js': ['coverage'],
-      // 'tests/**/*.js': ['babel']
+      'lib/**/*.js': ['babel'],
+      'tests/**/*.js': ['babel']
     },
     coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
+      dir: 'coverage',
+      includeAllSources: true,
+      reporters: [
+        { type: 'html', subdir: 'report-html' },
+        // { type: 'lcov', subdir: 'report-lcov' },
+        // { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' }
+      ]
     },
     port: 9876,
     colors: true,
