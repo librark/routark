cfdccdd364e8617a921ef01f6064fe1087f2b6b5
Esteban Echeverry <eecheverry@nubark.com>
2019-02-13 14:35:13 -0500

    Setup coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,33 @@
+const path = require('path')
+
 module.exports = function (config) {
   config.set({
+    basePath: '.',
     frameworks: ['jasmine'],
-    files: ['tests/**/*.js'],
-    reporters: ['progress'],
-    port: 9876, // karma web server port
+    files: [
+      {pattern: 'lib/**/*.js', type: 'module', watched: true, included: true, served: true},
+      {pattern: 'tests/**/*.js', type: 'module', watched: true, included: true, served: true}
+    ],
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-babel-preprocessor'
+    ],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'lib/**/*.js': ['coverage'],
+      // 'tests/**/*.js': ['babel']
+    },
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     browsers: ['ChromeHeadless'],
     autoWatch: false,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
+    singleRun: true,
     concurrency: Infinity
   })
 }
--- tests/routark.spec.js
+++ tests/routark.spec.js
@@ -2,7 +2,7 @@ import { Routark } from '../lib/routark.js'
 
 describe('Routark', function () {
   beforeEach(function () {
-    this.router = new Routark()
+    this.router = new Routark(window)
   })
 
   it('can be instantiated', function () {
