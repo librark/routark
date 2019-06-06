d9a687bcb9c806d6238bb3e6be6473454b12608b
eecheverry <eecheverry@nubark.com>
2019-06-06 11:29:59 -0500

    Cover match method

--- tests/routark.spec.js
+++ tests/routark.spec.js
@@ -57,8 +57,41 @@ describe('Routark', () => {
     await router._executePath('/base/')
     expect(called).toEqual('base')
     expect(router.current).toEqual('/base/')
+
     await router._executePath('/base/main')
     expect(called).toEqual('main')
     expect(router.current).toEqual('/base/main')
+  })
+
+  it('matches a source and a target path to get a split index', async () => {
+    let sourceList = null
+    let targetList = null
+
+    router._getSplitIndex = (_sourceList, _targetList) => {
+      sourceList = _sourceList
+      targetList = _targetList
+    }
+
+    await router._match('/base', '/base/media')
+    expect(sourceList).toEqual(['', 'base'])
+    expect(targetList).toEqual(['', 'base', 'media'])
+
+    await router._match('/base/report', '/base/report/detail/1')
+    expect(sourceList).toEqual(['', 'base', 'report'])
+    expect(targetList).toEqual(['', 'base', 'report', 'detail', '1'])
+  })
+
+  it('matches a source and a target path to get a split index', async () => {
+    let paths = []
+    router._executePath = async (_path) => {
+      paths.push(_path)
+    }
+
+    await router._match('/base', '/base/media')
+    expect(paths).toEqual(['/base/media'])
+
+    paths = []
+    await router._match('/base/report', '/base/report/detail/1')
+    expect(paths).toEqual(['/base/report/detail', '/base/report/detail/1'])
   })
 })
