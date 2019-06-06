6836b6aba64d3bec12631b8d2b7d5f0c5f5f3581
eecheverry <eecheverry@nubark.com>
2019-06-06 08:45:17 -0500

    Cover addRoutes

--- tests/routark.spec.js
+++ tests/routark.spec.js
@@ -9,4 +9,20 @@ describe('Routark', function () {
   it('can be instantiated', function () {
     expect(router).toBeTruthy()
   })
+
+  it('can add multiple routes to itself', () => {
+    const basePath = '/base/'
+    router.addRoutes(basePath, [
+      {
+        'path': '',
+        'action': async () => null
+      },
+      {
+        'path': 'main',
+        'action': async () => null
+      }
+    ])
+    expect(router._routes[0].path).toEqual('/base/')
+    expect(router._routes[1].path).toEqual('/base/main')
+  })
 })
