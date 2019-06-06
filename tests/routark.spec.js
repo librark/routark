dde63f2d8294470c61ec50749c7d1791a21e34ce
eecheverry <eecheverry@nubark.com>
2019-06-06 09:55:15 -0500

    Cover execute path method

--- tests/routark.spec.js
+++ tests/routark.spec.js
@@ -40,4 +40,25 @@ describe('Routark', () => {
       ['', 'base', 'history', 'detail', '3'])
     expect(splitIndex).toEqual(4)
   })
+
+  it('executes a given path by calling its registered action', async () => {
+    let called = ''
+    router.addRoutes('/base/', [
+      {
+        'path': '',
+        'action': async () => { called = 'base' }
+      },
+      {
+        'path': 'main',
+        'action': async () => { called = 'main' }
+      }
+    ])
+
+    await router._executePath('/base/')
+    expect(called).toEqual('base')
+    expect(router.current).toEqual('/base/')
+    await router._executePath('/base/main')
+    expect(called).toEqual('main')
+    expect(router.current).toEqual('/base/main')
+  })
 })
