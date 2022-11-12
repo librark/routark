ab247353b0127c68dde3df87032f6a35b7f9afc8
Esteban Echeverry <eecheverry@knowark.com>
2022-11-12 10:03:22 -0500

    Add type definitions.

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+export declare class Routark {
+  constructor(globalObject: object, root: string)
+
+  addRoutes(basePath: string, routes: { path: string }): void
+
+  move(): Promise<void>
+
+  navigate(path: string): Promise<void>
+}
